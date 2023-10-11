601a328ed2c0644a60d737bf937d58c6acde8c8d
indradeep2002 <indradeephalder@gmail.com>
2023-10-12 01:55:35 +0530

    PYTHON TUTORIALS

---
+++ MIT_EECS/6.01SC/PYTHON_TUTORIAL/py3_2_1.py
@@ -0,0 +1,15 @@
+def multIA(m, n):
+    i = 0
+    ans = 0
+
+    while i < n:
+        ans += m
+        i += 1
+
+    return ans
+
+print(multIA(1 , 2))
+print(multIA(2, 3))
+print(multIA(10, 9))
+
+    
