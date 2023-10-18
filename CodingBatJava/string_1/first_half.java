c081bde75d906ff3e5f7716b1adb107e364c0d0f
indradeep2002 <indradeephalder@gmail.com>
2023-10-18 20:01:04 +0530

    practice java on coding bat

---
+++ CodingBatJava/string_1/first_half.java
@@ -0,0 +1,25 @@
+package CodingBatJava.string_1;
+
+public class first_half {
+    public static String firstHalf(String str) {
+        int n = str.length();
+        int half = n / 2;
+        int i = 0;
+        String final_ans = "";
+        
+        while ( i < half){
+          final_ans += str.charAt(i);
+          i += 1;
+        }
+        
+        return final_ans;
+        
+    }
+
+    public static void main(String[] arguments){
+        
+        System.out.println(firstHalf("WooHoo"));
+        System.out.println(firstHalf("HelloThere"));
+        System.out.println(firstHalf("abcdef"));
+    }
+}
