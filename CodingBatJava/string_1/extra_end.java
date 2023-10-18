c037dd90ad883434e4b994ac75e4865589edbbba
indradeep2002 <indradeephalder@gmail.com>
2023-10-18 19:48:51 +0530

    practice java on codingbat

---
+++ CodingBatJava/string_1/extra_end.java
@@ -0,0 +1,22 @@
+package CodingBatJava.string_1;
+
+public class extra_end {
+    public static String extraEnd(String str) {
+        if(str.length() < 2){
+          return str;
+        }
+        int n = str.length();
+        String s = "";
+        
+        s += str.charAt(n - 2);
+        s += str.charAt(n -1);
+        
+        return s + s + s;
+    }
+
+    public static void main(String[] arguments){
+        System.out.println(extraEnd("Hello"));
+        System.out.println(extraEnd("ab"));
+        System.out.println(extraEnd("Hi"));
+    }
+}
