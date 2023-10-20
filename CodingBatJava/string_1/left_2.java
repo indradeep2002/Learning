fda04a2dcb2dfc174adac4fb27e2119affce1538
indradeep2002 <indradeephalder@gmail.com>
2023-10-20 18:00:03 +0530

    practice java on codingbat

---
+++ CodingBatJava/string_1/left_2.java
@@ -0,0 +1,29 @@
+package CodingBatJava.string_1;
+
+public class left_2 {
+    
+    public static String left2(String str) {
+        if(str.length() <= 2){
+          return str;
+        }
+        
+        String first_two = "";
+        String rest_str = "";
+        
+        first_two += str.charAt(0);
+        first_two += str.charAt(1);
+        
+        for(int i = 2; i < str.length(); i++){
+          rest_str += str.charAt(i);
+        }
+        
+        return rest_str + first_two;
+    }
+
+    public static void main(String[] arguments){
+
+        System.out.println(left2("Hello"));
+        System.out.println(left2("Java"));
+        System.out.println(left2("hi"));
+    }
+}
