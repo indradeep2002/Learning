72dcb618794cb41c9d2d4673b6365b76c2095285
indradeep2002 <indradeephalder@gmail.com>
2023-11-14 20:21:57 +0530

    practice java on codingbat

---
+++ CodingBatJava/array1/firstlast6.java
@@ -0,0 +1,33 @@
+public class firstlast6 {
+    
+
+    public static boolean firstLast6(int[] nums) {
+        int n = nums.length;
+        
+        if(n < 1){
+          return false;
+        }
+        else if(nums[0] == 6){
+          return true;
+        }
+        else if(nums[n-1] == 6){
+          return true;
+        }
+        else{
+          return false;
+        }
+    }
+
+
+    public static void main(String[] arguments){
+
+        int nums[] = {1 , 2, 6};
+        System.out.println(firstLast6(nums));
+
+        int nums2[] = {6, 1, 2, 3};
+        System.out.println(firstLast6(nums2));
+
+        int nums3[] = {13, 6, 1, 2, 3};
+        System.out.println(firstLast6(nums3));
+    }
+}
